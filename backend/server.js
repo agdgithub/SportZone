c6c308172db0692994bca7115d10b0794e3dba43
agdgithub <iit2020182@iiita.ac.in>
2024-03-06 16:18:22 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const __dirname = path.resolve()
 if(process.env.NODE_ENV ===  'production'){
     app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'forntend', 'build', 'index.html')))
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
 }else{
     app.get('/', (req, res) => {
         res.send('API is running...')
